78997fc862265da61dd3e20af6b3e9d8da31d62e
NoahLeichty <nleichty1@gmail.com>
2025-06-09 17:45:22 -0500

    Started making a basic calculator

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+//Will be a calculator that adds, subtracts, multiplies, and divides
+public class Main {
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+}
