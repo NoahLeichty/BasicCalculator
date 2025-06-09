dd17d31f015278c5676a52bc87145b215c6be410
NoahLeichty <nleichty1@gmail.com>
2025-06-09 18:32:15 -0500

    Made it ask for first number, operation, and second number. Uses if statement to determine correct operation.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,28 @@ import java.util.Scanner;
 public class Main {
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        long num;
-        System.out.println();
+
+        // This just does 2 numbers
+        // ToDo Make it take in more numbers
+        // ToDo make the UI and User experience better
+        System.out.println("Enter your first number: ");
+        double num1 = scan.nextDouble();
+        System.out.println("Enter what operation you want to do (+,-,*,/) :");
+        String operation = scan.next();
+        System.out.println("Enter your second number: ");
+        double num2 = scan.nextDouble();
+
+        // This checks what operation the user chose and does it
+        if (operation.startsWith("+")){
+            System.out.println(num1 + num2);
+        } else if (operation.startsWith("-")) {
+            System.out.println(num1 - num2);
+        } else if (operation.startsWith("*")) {
+            System.out.println(num1 * num2);
+        } else if (operation.startsWith("/")) {
+            System.out.println(num1 / num2);
+        }else {
+            System.out.println("Not a valid operation!");
+        }
     }
 }
