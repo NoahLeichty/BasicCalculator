a7dbe9063766c0932a16e6ffdfc25c5f886f4fea
NoahLeichty <nleichty1@gmail.com>
2025-06-12 16:06:03 -0500

    Added more buttons and tried to change the layout.

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,55 @@ public class Main {
         JFrame frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 400);
+
+        // Buttons for caclulator
+        frame.setLayout(new BorderLayout());
+        JButton buttonText = new JButton("");
+        JButton buttonC = new JButton("c");
+        JButton buttonBack = new JButton("Back");
+        JButton buttonAdd = new JButton("+");
+        JButton buttonSub = new JButton("-");
+        JButton buttonMult = new JButton("*");
+        JButton buttonDiv = new JButton("/");
+        JButton buttonDec = new JButton(".");
+        JButton buttonEquals = new JButton("=");
+        JButton button0 = new JButton("0");
+        JButton button1 = new JButton("1");
+        JButton button2 = new JButton("2");
+        JButton button3 = new JButton("3");
+        JButton button4 = new JButton("4");
+        JButton button5 = new JButton("5");
+        JButton button6 = new JButton("6");
+        JButton button7 = new JButton("7");
+        JButton button8 = new JButton("8");
+        JButton button9 = new JButton("9");
+
+        JPanel panel = new JPanel();
+
+        // Adding buttons to frame
+        frame.add(buttonText);
+        frame.add(buttonC);
+        frame.add(buttonBack);
+        frame.add(buttonAdd);
+        frame.add(buttonSub);
+        frame.add(buttonMult);
+        frame.add(buttonDiv);
+        frame.add(buttonDec);
+        frame.add(buttonEquals);
+        frame.add(button0);
+        frame.add(button1);
+        frame.add(button2);
+        frame.add(button3);
+        frame.add(button4);
+        frame.add(button5);
+        frame.add(button6);
+        frame.add(button7);
+        frame.add(button8);
+        frame.add(button9);
+
+        frame.setLayout(new GridLayout(5,4));
         frame.setVisible(true);
-        JButton button = new JButton("0");
-        button.setVisible(true);
+
 
         // This just does 2 numbers
         // ToDo Make it take in more numbers
